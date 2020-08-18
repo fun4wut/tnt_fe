442034733a1dfced0ca6c1e5e824d640f001e1b8
Houze Ni <t-hni@microsoft.com>
2020-08-18 12:08:07 +0800

    add timeout cookie

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,8 +9,11 @@ function fetchFromDHL(trackingNum: string): Promise<ShipHistory[]> {
     .query({ AWB: trackingNum })
     .set({
       Accept: "*/*",
-      Connection: "keep-alive"
+      Connection: "keep-alive",
+      "Accept-Encoding": "gzip, deflate, br",
+      Cookie: "bm_sz=F0DF143AFE4E3DA9E2C15B583EBE04F6~YAAQcugyF89e9/FzAQAALQa9/wiLOTlYOGGQcVLxVPq2nRvd69j0YzxrXkfoY7V2O3O/zg0XjqZWhUHnzSa8t5h+FuckJw7/lJypL0RM5Ha4eS+8NRFlHM0P+ywosfScZ1y/48vrjijQtiEyVXH0RhEobAEPLdxhe641SHU7dYYAnLP+tLSPr8emWx8N; TS016f3c0b=01914b743ddcb6a7940e9b5adca49773db7effd90fc6f2102aa2c2035a90fc8dad61b14a6dd79450315812764bd213afcee19c2c66; _abck=B757BF70FE97A6995B9A944976571CE9~-1~YAAQcugyF61f9/FzAQAAM2O+/wRnx5ug9bQKNnAgZhRV6fWnVF/k/GxVy7c40sU2a2mzIThOUE2OP7+oNNf0U/mvCfDCOVhZzH/sywLjazJCuyXab7nvOf53h+Vw8xPufTOZG/p88moCV1IXo/4XE2WdM91ns6A8N7te0EzJ4F6CNTn3oHK73eYQE+qNZQkUEzeJNWAA8h2N3Z7qHZQmcysnNmuSQyvi2FisMaWVNUlDFLpS7dQnJYU5js1DJTyYCUPXoveq4DDzzYZ/+qBH5i9KEgrYPsJmreaSn5MYm3i/viOX4gu4i2FK9Iu3Z4l43VA06g==~0~-1~-1"
     })
+    .timeout(3000)
     .then(res => JSON.parse(res.text).results[0].checkpoints)
     .then(list => list.map(evt => ({
       location: evt.location,
